38229ecd3a2e33928cdde22493f045373a0a5871
Julianna Granjeia <judinanina@gmail.com>
2023-03-13 22:31:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,19 @@ app = Flask(__name__)
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Julianna Granjeia)"
 
+menu="""
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> ! <a href="/contato">Contato</a>
+<br>
+"""
+
+@app.route("/")
+def hello_world():
+  return menu+"Olá, mundo! Esse é o meu site. (Julianna Granjeia)"
+
 @app.route("/sobre")
 def sobre():
-  return "Testando. Que coisa!"
+  return menu+"Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "judinanina@gmail.com"
+  return menu+"Aqui vai o conteúdo da página Contato"
