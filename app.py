58b126d739d1fa3129527a1f60437c537cf76eb4
Julianna Granjeia <judinanina@gmail.com>
2023-03-15 08:28:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Julianna Granjeia)"
 
-menu = """<p style="text-align: center; "><div style="background-color: black; border-radius: 5px; display: inline-block; padding: 10px;"><a href="/"><span style="color: rgb(255, 0, 0); font-family: &quot;Arial Black&quot;; font-size: 24px;">Página inicial</span></a> | <a href="/sobre"><span style="color: rgb(255, 0, 0); font-family: &quot;Arial Black&quot;; font-size: 24px;">Sobre</span></a> | <a href="/contato"><span style="color: rgb(255, 0, 0); font-family: &quot;Arial Black&quot;; font-size: 24px;">Contato</span></a></p></div><br>"""
+menu = """
+<a href="/">Página inicial</a> | <a href="/textos">Textos</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
 
 @app.route("/")
 def index():
